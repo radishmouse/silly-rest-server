2fe48243e733305293bf635973429c93cc06170d
Todd Gandee <tgandee@bignerdranch.com>
2015-08-26 11:24:06 -0400

    add JSONAPI

--- index.js
+++ index.js
@@ -4,8 +4,10 @@
 // Basic App setup --------------------
 var path = require('path');
 var express = require('express');
+var API = require('json-api');
 var app = express();
 var port = process.env.PORT || 1337;
+var APIError = API.types.Error;
 
 var makeRoutes = require('./router-maker');
 
@@ -23,16 +25,35 @@ var mongoose = require('mongoose');
 mongoose.connect(MONGO_URI);
 
 // Our models
-var MenuItem = require('./models/menuitem');
-var Creature = require('./models/creature');
-var Cryptid = require('./models/cryptid');
-var Sighting = require('./models/sighting');
-var Witness = require('./models/witness');
+var models = {
+    MenuItem : require('./models/menuitem'),
+    Creature : require('./models/creature'),
+    Cryptid : require('./models/cryptid'),
+    Sighting : require('./models/sighting'),
+    Witness : require('./models/witness')
+};
+
+// Adapter for JSON-API
+var adapter = new API.dbAdapters.Mongoose(models);
+var registry = new API.ResourceTypeRegistry();
+var Controller = new API.controllers.API(registry);
 
 
-// Routing --------------------------------
+["sightings", "cryptids", "witnesses"].forEach(function(resourceType) {
+  var description = require('./resource-descriptions/' + resourceType);
+  description.dbAdapter = adapter;
+  registry.type(resourceType, description);
+})
+
+var Docs = new API.controllers.Documentation(registry, {name: 'Tracker API'});
+
+// Initialize the express app + front controller.
+var app = express();
+
+var Front = new API.httpStrategies.Express(Controller, Docs);
+var apiReqHandler = Front.apiRequest.bind(Front);
+
 var router = express.Router();
-
 // some really basic logging middleware
 router.use(function (req, res, next) {
     console.log('Accessing ' + req.path);
@@ -44,24 +65,65 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-
     next();
 });
 
-router.get('/', function (req, res) {
-    res.json({message: 'so say we all'});
+// Now, add the routes.
+// To do this in a more scalable and configurable way, check out
+// http://github.com/ethanresnick/express-simple-router. To protect some
+// routes, check out http://github.com/ethanresnick/express-simple-firewall.
+app.get("/", Front.docsRequest.bind(Front));
+app.route("/api/:type(sightings|cryptids|witnesses)")
+  .get(apiReqHandler).post(apiReqHandler).patch(apiReqHandler);
+app.route("/api/:type(sightings|cryptids|witnesses)/:id")
+  .get(apiReqHandler).patch(apiReqHandler).delete(apiReqHandler);
+app.route("/api/:type(sightings|cryptids|witnesses)/:id/relationships/:relationship")
+  .get(apiReqHandler).post(apiReqHandler).patch(apiReqHandler);
+
+app.use(function(req, res, next) {
+  Front.sendError(new APIError(404, undefined, 'Not Found'), req, res);
 });
+
+// And we're done! Start 'er up!
+app.listen(port, '0.0.0.0');
+console.log('Now you are http://localhost:' + port);
+
+
+
+
+
+// // Routing --------------------------------
+// var router = express.Router();
+
+// // some really basic logging middleware
+// router.use(function (req, res, next) {
+//     console.log('Accessing ' + req.path);
+//     next();
+// });
+
+// // This is for cross domain fun
+// app.all('*', function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+
+//     next();
+// });
+
+// router.get('/', function (req, res) {
+//     res.json({message: 'so say we all'});
+// });
 
 
 // Dynamically create routes for models
-makeRoutes(router, MenuItem, 'menuitem');
-makeRoutes(router, Creature, 'creature');
-makeRoutes(router, Cryptid, 'cryptid');
-makeRoutes(router, Sighting, 'sighting');
-makeRoutes(router, Witness, 'witness', 'witnesses');
+// makeRoutes(router, MenuItem, 'menuitem');
+// makeRoutes(router, Creature, 'creature');
+// makeRoutes(router, Cryptid, 'cryptid');
+// makeRoutes(router, Sighting, 'sighting');
+// makeRoutes(router, Witness, 'witness', 'witnesses');
 
 
 // Fire it up! Fire it up!------------------------------
-app.use('/api', router);
-app.listen(port, '0.0.0.0');
-console.log('Now you are 1337: ' + port);
+// app.use('/api', router);
+// app.listen(port, '0.0.0.0');
+// console.log('Now you are 1337: ' + port);
---
+++ resource-descriptions/sightings.js
@@ -0,0 +1,6 @@
+module.exports = {
+  urlTemplates: {
+    "self": "/api/sightings/{id}",
+    "relationship": "/api/sightings/{ownerId}/relationships/{path}"
+  }
+};
