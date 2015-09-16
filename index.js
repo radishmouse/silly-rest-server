4fbb5886f4057ca42392eaaf9a5c27a6aa88b32c
Todd Gandee <tgandee@bignerdranch.com>
2015-09-16 19:57:03 -0400

    add options type to the cors catch-all

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ router.use(function (req, res, next) {
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
 
