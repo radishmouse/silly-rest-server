7559a42bec2202c8d60d44b1e8d5e8079a1d2b53
Todd Gandee <tgandee@bignerdranch.com>
2015-09-16 19:53:07 -0400

    trying to fix one more time

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ router.use(function (req, res, next) {
 });
 
 // This is for cross domain fun
-app.use('*', function(req, res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
